356bbf82cc106e42d38d0b427c03e03dc63cb5ab
Erfan <erfannavab@gmail.com>
2022-10-12 09:17:39 +0330

    added repositories.

---
+++ src/main/java/nvb/springframework/recipeapp/repository/CategoryRepository.java
@@ -0,0 +1,9 @@
+package nvb.springframework.recipeapp.repository;
+
+import nvb.springframework.recipeapp.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
---
+++ src/main/java/nvb/springframework/recipeapp/repository/UnitOfMeasureRepository.java
@@ -0,0 +1,9 @@
+package nvb.springframework.recipeapp.repository;
+
+import nvb.springframework.recipeapp.model.UnitOfMeasure;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UnitOfMeasureRepository extends JpaRepository<UnitOfMeasure, Long> {
+}
